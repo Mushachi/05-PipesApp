7cbe77b03cb5e886d701edb87a66f11c9ed1ca13
Julio Barriga <mushachi@gmail.com>
2022-11-17 08:34:31 -0600

    PipeApp v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PipesApp';
+
+  nombre: string = 'juLio BarRiGa';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Julio'
+  }
+
+  mostrarNombre() {
+    console.log( this.nombre );
+    console.log( this.valor );
+    console.log( this.obj );
+  }
+
 }
