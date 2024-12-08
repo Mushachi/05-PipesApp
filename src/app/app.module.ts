c77260b93bea2d3477c5977964a217b69e5b9307
Julio Barriga <mushachi@gmail.>
2024-12-08 08:37:20 -0600

    Cambiar idioma global y manualmente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppRoutingModule } from './app-routing.module'
@@ -7,6 +7,15 @@ import { AppComponent } from './app.component';
 
 import { SharedModule } from './shared/shared.module';
 import { PrimeNgModule } from './prime-ng/prime-ng.module';
+
+// Configuracion del locale de la app
+import localeEsMX from '@angular/common/locales/es-MX'
+import localeRu from '@angular/common/locales/ru'
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsMX );
+registerLocaleData( localeRu );
 
 @NgModule({
   declarations: [
@@ -19,7 +28,9 @@ import { PrimeNgModule } from './prime-ng/prime-ng.module';
     AppRoutingModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-MX' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
