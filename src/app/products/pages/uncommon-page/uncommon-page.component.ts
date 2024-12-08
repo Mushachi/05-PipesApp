bbf8a3983dcb13efa9212a3651e850bce4f25d2b
Julio Barriga <mushachi@gmail.>
2024-12-08 15:29:56 -0600

    AsyncPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -44,4 +45,16 @@ export class UncommonPageComponent implements OnInit {
     age: 42,
     address: 'Tonala, Jalisco'
   }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:', value) ),
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data en la promesa.' );
+    }, 3500);
+  })
+
 }
